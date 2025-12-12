2ef829a1f57f7ab267b318045cd860737ffa72e5
Uraraka-ai <vovagava716@gmail.com>
2025-12-12 18:53:09 +0200

    update service plan files

--- task05/modules/app_service_plan/main.tf
+++ task05/modules/app_service_plan/main.tf
@@ -2,14 +2,8 @@ resource "azurerm_service_plan" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "Windows"
-  reserved            = false
-
-  sku {
-    tier     = "Standard"
-    size     = var.sku
-    capacity = var.worker_count
-  }
-
-  tags = var.tags
+  os_type             = "Windows"                # or "Linux" if you need Linux
+  sku_name            = var.sku                  # e.g., "S1"
+  worker_count        = var.worker_count
+  tags                = var.tags
 }
