c19f049ba1d9f6d6a2d59ba882a131c975db7e93
Uraraka-ai <vovagava716@gmail.com>
2025-12-12 18:55:27 +0200

    add service main tf

--- task05/modules/app_service_plan/main.tf
+++ task05/modules/app_service_plan/main.tf
@@ -2,8 +2,8 @@ resource "azurerm_service_plan" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  os_type             = "Windows"                # or "Linux" if you need Linux
-  sku_name            = var.sku                  # e.g., "S1"
+  os_type             = "Windows" # or "Linux" if you need Linux
+  sku_name            = var.sku   # e.g., "S1"
   worker_count        = var.worker_count
   tags                = var.tags
 }
