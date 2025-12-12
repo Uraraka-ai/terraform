355c5688632ee8dab0c5d454933090ca1350621f
Uraraka-ai <vovagava716@gmail.com>
2025-12-12 18:26:11 +0200

    fmt traffic main

--- task05/modules/traffic_manager/main.tf
+++ task05/modules/traffic_manager/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_traffic_manager_profile" "this" {
-  name                    = var.name
-  resource_group_name     = var.resource_group_name
-  profile_status          = "Enabled"
-  traffic_routing_method  = var.routing_method
+  name                   = var.name
+  resource_group_name    = var.resource_group_name
+  profile_status         = "Enabled"
+  traffic_routing_method = var.routing_method
 
   dns_config {
     relative_name = var.name
@@ -21,9 +21,9 @@ resource "azurerm_traffic_manager_profile" "this" {
 resource "azurerm_traffic_manager_azure_endpoint" "app_service" {
   for_each = var.endpoints
 
-  name                = each.value.name
-  profile_id          = azurerm_traffic_manager_profile.this.id
-  target_resource_id  = each.value.target_resource_id
-  priority            = each.value.priority
-  weight              = each.value.weight
+  name               = each.value.name
+  profile_id         = azurerm_traffic_manager_profile.this.id
+  target_resource_id = each.value.target_resource_id
+  priority           = each.value.priority
+  weight             = each.value.weight
 }
